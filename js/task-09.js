6f6e6f7a894221b1e1afb56d80d489e9d10fab45
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-03-23 15:57:13 +0100

    Change in the hw-06

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const color = document.querySelector("body");
+
+const text_color = document.querySelector(".color");
+
+const button = document.querySelector(".change-color"); 
+
+
+
+button.addEventListener("click",() => {
+  color.style.backgroundColor = getRandomHexColor();
+  text_color.textContent = getRandomHexColor();
+});
