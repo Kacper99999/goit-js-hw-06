b890b9c3807be6253a9a035e9c23864b5a838fb8
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-03-23 12:07:34 +0100

    Change in the hw-06

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+let counterValue = 0;
+
+const decrement = document.querySelector("[data-action = 'decrement']");
+
+const inctement = document.querySelector("[data-action = 'increment']");
+
+const value = document.querySelector("#value");
+
+decrement.addEventListener("click",() => {
+  counterValue -= 1;
+  value.innerHTML = `<span id='value'>${counterValue}</span>`
+});
+
+inctement.addEventListener("click",() => {
+  counterValue += 1;
+  value.innerHTML = `<span id='value'>${counterValue}</span>`;
+});
