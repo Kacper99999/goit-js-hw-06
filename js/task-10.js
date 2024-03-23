ab5f874131a91d6081927ead8095d431fc3645e8
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-03-23 17:39:24 +0100

    Change in the hw-06

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const amount = document.querySelector("input");
+
+const create = document.querySelector("[data-create]");
+
+const destroy = document.querySelector("[data-destroy]");
+
+const box = document.querySelector("#boxes");
+
+
+
+create.addEventListener("click", function() {
+  createBoxes(parseInt(amount.value));
+});
+
+destroy.addEventListener("click", function() {
+  destroyBoxes();
+});
+
+
+function createBoxes(amount){
+  let element;
+    for(let i = 0; i < amount; i++){
+      element = document.createElement("div");
+      box.appendChild(element);
+      element.style.width = `${30 + (i *10)}px`;
+      element.style.height = `${30 + (i *10)}px`;
+      element.style.backgroundColor = getRandomHexColor();
+}};
+
+function destroyBoxes(){
+  box.innerHTML = "";
+};
