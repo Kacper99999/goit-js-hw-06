f127b4b1ae47bcca69beea6242b916b435ca314f
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-03-23 15:31:44 +0100

    Change in the hw-06

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+function handleSubmit(event){
+  event.preventDefault();
+
+  const obj = {};
+
+  const target = event.target;
+
+  const valueEmail = target.elements.email.value;
+  const valuePassword = target.elements.password.value;
+
+  obj.email = valueEmail;
+  obj.password = valuePassword;
+
+  if(valueEmail === "" || valuePassword === ""){
+    alert("Please fill in all the fields!");
+  }
+
+  console.log(obj);
+  form.reset();
+};
+
+form.addEventListener("submit",handleSubmit);
